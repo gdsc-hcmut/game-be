f1f7fad3a772b52ee6a6f93b1c7fa8aad456466d
lygioian <lygioian@gmail.com>
2023-02-22 22:22:55 +0700

    d

--- src/controllers/discord.controller.ts
+++ src/controllers/discord.controller.ts
@@ -48,11 +48,11 @@ export class DiscordController extends Controller {
             '/private/users/:discordId',
             this.getUserInfo.bind(this),
         );
+        this.router.get('/private/board', this.discordLeaderboard.bind(this));
         this.router.post('/private/daily', this.discordDaily.bind(this));
         this.router.post('/private/work', this.discordWork.bind(this));
         this.router.post('/private/battle/start', this.startBattle.bind(this));
         this.router.post('/private/battle/end', this.endBattle.bind(this));
-        this.router.post('/private/board', this.discordLeaderboard.bind(this));
     }
 
     async getDiscordActivityInformation(req: Request, res: Response) {
