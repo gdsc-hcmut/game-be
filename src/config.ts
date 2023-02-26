c5a7c875d334ace09b9a11f9c21e885ff53dcae0
lygioian <lygioian@gmail.com>
2023-02-26 12:22:15 +0700

    e

--- src/config.ts
+++ src/config.ts
@@ -50,6 +50,7 @@ export const USER_WHITE_LIST: Array<{ email: string }> = [
     { email: 'minhduytranct2017@mail.com' },
     { email: 'minh.tranduy2209@hcmut.edu.vn' },
     { email: 'gdscchatapp@gmail.com' },
+    { email: 'duc.nv291@gmail.com' },
 ];
 
 export const DB_NAME = 'bugs';
