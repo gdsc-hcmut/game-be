cdd786389aa47ee59bcb5b8d5a51ad6366edf4cd
lygioian <lygioian@gmail.com>
2023-05-22 11:02:28 +0700

    open session

--- src/app.ts
+++ src/app.ts
@@ -44,13 +44,13 @@ class App {
         this.app.use('/static', express.static(STATIC_DIR));
 
         middlewares.forEach((m) => this.app.use(m));
-        // this.app.use(passport.session());
-        // this.app.use(
-        //     cookieSession({
-        //         maxAge: 24 * 60 * 60 * 1000,
-        //         keys: [COOKIE_KEY],
-        //     }),
-        // );
+        this.app.use(passport.session());
+        this.app.use(
+            cookieSession({
+                maxAge: 24 * 60 * 60 * 1000,
+                keys: [COOKIE_KEY],
+            }),
+        );
     }
 
     public applyExternalMiddleware(middleware: any) {
