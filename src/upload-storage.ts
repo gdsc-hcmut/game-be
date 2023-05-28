69d12cd45af446fa4e88a24bb3241a2b634e4720
Quoc Hung <truongquochung312@gmail.com>
2023-05-28 12:36:44 +0700

    set file size limit to 50mb

--- src/upload-storage.ts
+++ src/upload-storage.ts
@@ -4,4 +4,9 @@ import multer from "multer";
  * Defines global file uploader object for multer
  */
 
-export const fileUploader = multer({ storage: multer.memoryStorage() });
+export const fileUploader = multer({
+    storage: multer.memoryStorage(),
+    limits: {
+        fieldSize: 50 * 1024 * 1024
+    }
+});
