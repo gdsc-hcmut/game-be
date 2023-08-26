aabcc567647b5b11cd96c5b99acfd445f62801d0
Hieu Tran <ngoquochieu1211@gmail.com>
2023-08-26 12:47:18 +0700

    chore: add map_1 (to be continue)

--- src/constant/maze/map/index.ts
+++ src/constant/maze/map/index.ts
@@ -164,3 +164,510 @@ export const initMapLevel5: CellObject[] = [
         isHidden: false,
     },
 ];
+
+export const initMapClubDay: CellObject[] = [
+    {
+        property: CellType.Wall, // (0,0)
+        isHidden: false,
+    },
+    {
+        property: CellType.Wall, // (1,0)
+        isHidden: false,
+    },
+    {
+        property: CellType.Wall, // (2,0)
+        isHidden: false,
+    },
+    {
+        property: CellType.Wall, // (3,0)
+        isHidden: false,
+    },
+    {
+        property: CellType.Wall, // (4,0)
+        isHidden: false,
+    },
+    {
+        property: CellType.Wall, // (5,0)
+        isHidden: false,
+    },
+    {
+        property: CellType.Wall, // (6,0)
+        isHidden: false,
+    },
+    {
+        property: CellType.Wall, // (7,0)
+        isHidden: false,
+    },
+    {
+        property: CellType.Wall, // (8,0)
+        isHidden: false,
+    },
+    {
+        property: CellType.Wall, // (9,0)
+        isHidden: false,
+    },
+    {
+        property: CellType.Wall, // (10,0)
+        isHidden: false,
+    },
+    {
+        property: CellType.Wall, // (11,0)
+        isHidden: false,
+    },
+    {
+        property: CellType.Wall, // (12,0)
+        isHidden: false,
+    },
+    {
+        property: CellType.Wall, // (13,0)
+        isHidden: false,
+    },
+    {
+        property: CellType.Wall, // (14,0)
+        isHidden: false,
+    },
+    {
+        property: CellType.Wall, // (15,0)
+        isHidden: false,
+    },
+    {
+        property: CellType.Wall, // (16,0)
+        isHidden: false,
+    },
+    {
+        property: CellType.Wall, // (17,0)
+        isHidden: false,
+    },
+    {
+        property: CellType.Wall, // (18,0)
+        isHidden: false,
+    },
+    {
+        property: CellType.Wall, // (19,0)
+        isHidden: false,
+    },
+    {
+        property: CellType.Wall, // (20,0)
+        isHidden: false,
+    },
+    {
+        property: CellType.Wall, // (21,0)
+        isHidden: false,
+    },
+    {
+        property: CellType.Wall, // (22,0)
+        isHidden: false,
+    },
+    {
+        property: CellType.Wall, // (23,0)
+        isHidden: false,
+    },
+    {
+        property: CellType.Wall, // (24,0)
+        isHidden: false,
+    },
+    {
+        property: CellType.Wall, // (25,0)
+        isHidden: false,
+    },
+    {
+        property: CellType.Wall, // (26,0)
+        isHidden: false,
+    },
+    {
+        property: CellType.Wall, // (27,0)
+        isHidden: false,
+    },
+    {
+        property: CellType.Wall, // (28,0)
+        isHidden: false,
+    },
+    {
+        property: CellType.Wall, // (29,0)
+        isHidden: false,
+    },
+    {
+        property: CellType.Wall, // (30,0)
+        isHidden: false,
+    },
+    {
+        property: CellType.Wall, // (0,1)
+        isHidden: false,
+    },
+    {
+        property: CellType.Path, // (1,1)
+        isHidden: false,
+    },
+    {
+        property: CellType.Path, // (2,1)
+        isHidden: false,
+    },
+    {
+        property: CellType.Path, // (3,1)
+        isHidden: false,
+    },
+    {
+        property: CellType.Wall, // (4,1)
+        isHidden: false,
+    },
+    {
+        property: CellType.HpPortion, // (5,1)
+        isHidden: false,
+        hp: 100,
+    },
+    {
+        property: CellType.Lava, // (6,1)
+        isHidden: false,
+    },
+    {
+        property: CellType.Lava, // (7,1)
+        isHidden: false,
+    },
+    {
+        property: CellType.Lava, // (8,1)
+        isHidden: false,
+    },
+    {
+        property: CellType.Lava, // (9,1)
+        isHidden: false,
+    },
+    {
+        property: CellType.Lava, // (10,1)
+        isHidden: false,
+    },
+    {
+        property: CellType.Lava, // (11,1)
+        isHidden: false,
+    },
+    {
+        property: CellType.Lava, // (12,1)
+        isHidden: false,
+    },
+    {
+        property: CellType.Lava, // (13,1)
+        isHidden: false,
+    },
+    {
+        property: CellType.Lava, // (14,1)
+        isHidden: false,
+    },
+    {
+        property: CellType.Lava, // (15,1)
+        isHidden: false,
+    },
+    {
+        property: CellType.Lava, // (16,1)
+        isHidden: false,
+    },
+    {
+        property: CellType.Path, // (17,1)
+        isHidden: false,
+    },
+    {
+        property: CellType.Path, // (18,1)
+        isHidden: false,
+    },
+    {
+        property: CellType.Path, // (19,1)
+        isHidden: false,
+    },
+    {
+        property: CellType.Path, // (20,1)
+        isHidden: false,
+    },
+    {
+        property: CellType.Path, // (21,1)
+        isHidden: false,
+    },
+    {
+        property: CellType.Path, // (22,1)
+        isHidden: false,
+    },
+    {
+        property: CellType.Path, // (23,1)
+        isHidden: false,
+    },
+    {
+        property: CellType.Path, // (24,1)
+        isHidden: false,
+    },
+    {
+        property: CellType.Portal, // (25,1)
+        isHidden: false,
+        to: 31 * 16 + 27,
+    },
+    {
+        property: CellType.Wall, // (26,1)
+        isHidden: false,
+    },
+    {
+        property: CellType.Path, // (27,1)
+        isHidden: false,
+    },
+    {
+        property: CellType.Path, // (28,1)
+        isHidden: false,
+    },
+    {
+        property: CellType.End, // (29,1)
+        isHidden: false,
+    },
+    {
+        property: CellType.Wall, // (30,1)
+        isHidden: false,
+    },
+    {
+        property: CellType.Wall, // (0,2)
+        isHidden: false,
+    },
+    {
+        property: CellType.Path, // (1,2)
+        isHidden: false,
+    },
+    {
+        property: CellType.Wall, // (2,2)
+        isHidden: false,
+    },
+    {
+        property: CellType.Path, // (3,2)
+        isHidden: false,
+    },
+    {
+        property: CellType.Wall, // (4,2)
+        isHidden: false,
+    },
+    {
+        property: CellType.HpPortion, // (5,2)
+        isHidden: false,
+        hp: 100,
+    },
+    {
+        property: CellType.Wall, // (6,2)
+        isHidden: false,
+    },
+    {
+        property: CellType.Wall, // (7,2)
+        isHidden: false,
+    },
+    {
+        property: CellType.Wall, // (8,2)
+        isHidden: false,
+    },
+    {
+        property: CellType.Wall, // (9,2)
+        isHidden: false,
+    },
+    {
+        property: CellType.Wall, // (10,2)
+        isHidden: false,
+    },
+    {
+        property: CellType.Path, // (11,2)
+        isHidden: false,
+    },
+    {
+        property: CellType.Lava, // (12,2)
+        isHidden: false,
+    },
+    {
+        property: CellType.Lava, // (13,2)
+        isHidden: false,
+    },
+    {
+        property: CellType.Lava, // (14,2)
+        isHidden: false,
+    },
+    {
+        property: CellType.Lava, // (15,2)
+        isHidden: false,
+    },
+    {
+        property: CellType.Lava, // (16,2)
+        isHidden: false,
+    },
+    {
+        property: CellType.Path, // (17,2)
+        isHidden: false,
+    },
+    {
+        property: CellType.Wall, // (18,2)
+        isHidden: false,
+    },
+    {
+        property: CellType.Wall, // (19,2)
+        isHidden: false,
+    },
+    {
+        property: CellType.Wall, // (20,2)
+        isHidden: false,
+    },
+    {
+        property: CellType.Wall, // (21,2)
+        isHidden: false,
+    },
+    {
+        property: CellType.Wall, // (22,2)
+        isHidden: false,
+    },
+    {
+        property: CellType.Wall, // (23,2)
+        isHidden: false,
+    },
+    {
+        property: CellType.Wall, // (24,2)
+        isHidden: false,
+    },
+    {
+        property: CellType.Lava, // (25,2)
+        isHidden: false,
+        to: 31 * 16 + 27,
+    },
+    {
+        property: CellType.Lava, // (26,2)
+        isHidden: false,
+    },
+    {
+        property: CellType.Lava, // (27,2)
+        isHidden: false,
+    },
+    {
+        property: CellType.Lava, // (28,2)
+        isHidden: false,
+    },
+    {
+        property: CellType.Path, // (29,2)
+        isHidden: false,
+    },
+    {
+        property: CellType.Wall, // (30,2)
+        isHidden: false,
+    },
+    {
+        property: CellType.Wall, // (0,3)
+        isHidden: false,
+    },
+    {
+        property: CellType.Path, // (1,3)
+        isHidden: false,
+    },
+    {
+        property: CellType.Wall, // (2,3)
+        isHidden: false,
+    },
+    {
+        property: CellType.Portal, // (3,3)
+        isHidden: false,
+        to: 31 * 17 + 27, // to (27,17)
+    },
+    {
+        property: CellType.Wall, // (4,3)
+        isHidden: false,
+    },
+    {
+        property: CellType.Path, // (5,3)
+        isHidden: false,
+    },
+    {
+        property: CellType.Path, // (6,3)
+        isHidden: false,
+    },
+    {
+        property: CellType.Path, // (7,3)
+        isHidden: false,
+    },
+    {
+        property: CellType.Path, // (8,3)
+        isHidden: false,
+    },
+    {
+        property: CellType.Path, // (9,3)
+        isHidden: false,
+    },
+    {
+        property: CellType.Wall, // (10,3)
+        isHidden: false,
+    },
+    {
+        property: CellType.Path, // (11,3)
+        isHidden: false,
+    },
+    {
+        property: CellType.Lava, // (12,3)
+        isHidden: false,
+    },
+    {
+        property: CellType.Lava, // (13,3)
+        isHidden: false,
+    },
+    {
+        property: CellType.Lava, // (14,3)
+        isHidden: false,
+    },
+    {
+        property: CellType.Lava, // (15,3)
+        isHidden: false,
+    },
+    {
+        property: CellType.Lava, // (16,3)
+        isHidden: false,
+    },
+    {
+        property: CellType.Path, // (17,3)
+        isHidden: false,
+    },
+    {
+        property: CellType.Path, // (18,3)
+        isHidden: false,
+    },
+    {
+        property: CellType.Dragon, // (19,3)
+        isHidden: false,
+        attack: 500,
+        stamina: 10,
+    },
+    {
+        property: CellType.Path, // (20,3)
+        isHidden: false,
+    },
+    {
+        property: CellType.Lava, // (21,3)
+        isHidden: false,
+    },
+    {
+        property: CellType.Lava, // (22,3)
+        isHidden: false,
+    },
+    {
+        property: CellType.Lava, // (23,3)
+        isHidden: false,
+    },
+    {
+        property: CellType.Lava, // (24,3)
+        isHidden: false,
+    },
+    {
+        property: CellType.Lava, // (25,3)
+        isHidden: false,
+        to: 31 * 16 + 27,
+    },
+    {
+        property: CellType.Lava, // (26,3)
+        isHidden: false,
+    },
+    {
+        property: CellType.Lava, // (27,3)
+        isHidden: false,
+    },
+    {
+        property: CellType.Lava, // (28,3)
+        isHidden: false,
+    },
+    {
+        property: CellType.Path, // (29,3)
+        isHidden: false,
+    },
+    {
+        property: CellType.Wall, // (30,3)
+        isHidden: false,
+    },
+];
